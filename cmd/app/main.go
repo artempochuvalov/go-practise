db5249237d18d935ee7d3e694fee2418676aa308
artempochuvalov <artempochuvalov@gmail.com>
2026-07-31 20:32:46 +0300

    graceful shutdown added

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"context"
+	"errors"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"go-service/internal/config"
 	"go-service/internal/database"
@@ -15,13 +19,13 @@ import (
 func main() {
 	log := logger.NewLogger()
 
-	config, err := config.Load()
+	cfg, err := config.Load()
 	if err != nil {
-		log.Error("failed to get project envirenments", "eror", err.Error())
+		log.Error("failed to get project environments", "error", err)
 		os.Exit(1)
 	}
 
-	conn, err := database.NewDB(config.DatabaseURL)
+	conn, err := database.NewDB(cfg.DatabaseURL)
 	if err != nil {
 		log.Error("failed to connect to database", "error", err)
 		os.Exit(1)
@@ -47,10 +51,49 @@ func main() {
 	recoveredMux := middlewares.RecoverMiddleware(log)(mux)
 	loggedMux := middlewares.LoggingMiddleware(log)(recoveredMux)
 
-	log.Info("starting http server", "address", config.Port)
+	log.Info("starting http server", "address", cfg.Port)
 
-	if err = http.ListenAndServe(config.Port, loggedMux); err != nil {
-		log.Error("http server stopped", "error", err)
-		os.Exit(1)
+	server := &http.Server{
+		Addr:    cfg.Port,
+		Handler: loggedMux,
+	}
+
+	serverErr := make(chan error, 1)
+
+	go func() {
+		err := server.ListenAndServe()
+
+		if err != nil && !errors.Is(err, http.ErrServerClosed) {
+			serverErr <- err
+			return
+		}
+	}()
+
+	signalContext, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
+	select {
+	case <-signalContext.Done():
+		log.Info("shutdown signal received")
+
+		shutdownContext, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+
+		log.Info("shutting down server")
+		shutdownStart := time.Now()
+
+		if err := server.Shutdown(shutdownContext); err != nil {
+			log.Error("unable to shutdown", "error", err)
+
+			if closeErr := server.Close(); closeErr != nil {
+				log.Error("unable to force close server", "error", closeErr)
+			}
+		}
+
+		shutdownDuration := time.Since(shutdownStart)
+		log.Info("server shutdown completed", "duration", shutdownDuration)
+
+	case err := <-serverErr:
+		log.Error("server failed", "error", err)
 	}
 }
